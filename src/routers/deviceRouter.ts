fc66f87d7acc168d0d8d7e0dbf7615198e0e6f79
cchampou <clement@champouillon.com>
2022-12-24 17:54:56 +0100

    feat: give tokens for devices

--- src/routers/deviceRouter.ts
+++ src/routers/deviceRouter.ts
@@ -1,19 +1,25 @@
 import logger from '../utils/logger';
 import User from '../entities/User';
 import { Router } from 'express';
+import { sign } from 'jsonwebtoken';
 
 const deviceRouter = Router();
 
 deviceRouter.get('/check/:deviceId', async (req, res) => {
+  if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined');
   const { deviceId } = req.params;
   const user = await User.findOne({ deviceId: deviceId });
   if (user) {
-    return res.send({ message: 'Device found' });
+    const token = sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '10m',
+    });
+    return res.send({ token });
   }
   return res.status(404).send({ message: 'Device not found' });
 });
 
 deviceRouter.post('/register', async (req, res) => {
+  if (!process.env.JWT_SECRET) throw new Error('JWT_SECRET is not defined');
   const { registrationToken, deviceId } = req.body;
   logger.debug(
     `Registering user with token: ${registrationToken} and device id: ${deviceId}`,
@@ -25,7 +31,10 @@ deviceRouter.post('/register', async (req, res) => {
   }
   user.deviceId = deviceId;
   await user.save();
-  res.send({ message: 'User registered' });
+  const token = sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: '10m',
+  });
+  return res.send({ token });
 });
 
 export default deviceRouter;
